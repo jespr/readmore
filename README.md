b4febd9eec2d1f7fdf6ead199bbc63da4d928399
Jesper Christiansen <jchristiansen@zendesk.com>
2013-12-11 14:33:18 -0800

    First attempt at readmore plugin

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-readmore
-========
+# readmore
 
-Readmore is a jQuery plugin that truncates text given a limit, adds a 'Read more' link which will show the full text when clicked
+
+Readmore is a jQuery plugin that truncates text given a limit, adds a 
+'Read more' link which will show the full text when clicked. The expanded text 
+will have a 'Read less' link at the end, which will hide the expanded text when 
+clicked.
+
+
+## Example
+
+```html
+<div class="comment">
+  Now, this is a story all about how. My life got flipped-turned upside down. 
+  And I'd like to take a minute. Just sit right there. I'll tell you how I became 
+  the prince of a town called Bel Air
+</div>
+```
+
+```javascript
+$('.comment').readmore({ limit: 50 });
+```
---
+++ readmore.js
@@ -0,0 +1,106 @@
+/*global jQuery */
+/*
+ * Readmore plugin for jQuery
+ * Creator: Jesper Christiansen
+ * Project home: https://github.com/jespr/readmore
+ * Licensed under the Apache license
+ */
+
+;(function($) {
+
+  var defaults = {
+    limit: 100,
+    moreLinkText: 'Read more',
+    lessLinkText: 'Read less',
+    ellipsis: '&hellip;'
+  };
+
+  function Readmore(element, options) {
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
+
+    this.init();
+  }
+
+  Readmore.prototype = {
+
+    init: function() {
+      var $this = this;
+
+      $(this.element).each(function() {
+        var element = $(this);
+
+        if (element.html().length > $this.options.limit) {
+          $this.truncate(element);
+          $this.setupReadMoreLink(element);
+          $this.setupReadLessLink(element);
+        }
+      });
+    },
+
+    truncate: function(element) {
+      var truncatedText = $.trim(element.html().substring(0,this.options.limit));
+      var hiddenText = this.hideHiddenText(element);
+      var readMoreLink = ' <a href="#" class="read-more">' +
+        this.options.moreLinkText +
+        '</a>';
+
+      element.html(truncatedText + '<span class="read-more-hellip">' +
+          this.options.ellipsis +
+          '</span>');
+      element.append(readMoreLink);
+      element.append(hiddenText);
+    },
+
+    hideHiddenText: function(element) {
+      var readLessLink = '<a href="#" class="read-less">' +
+        this.options.lessLinkText +
+        '</a>';
+      var hiddenContainer = '<span class="read-more-hidden" style="display: none;">' +
+        element.html().substring(this.options.limit+1, element.html().length) +
+        " " +
+        readLessLink + "</span>";
+
+      return hiddenContainer;
+    },
+
+    setupReadMoreLink: function(element) {
+      var that = this;
+      element.find('.read-more').on('click', function(e) {
+        e.preventDefault();
+        that.showFullText(element, $(this));
+      });
+    },
+
+    setupReadLessLink: function(element) {
+      var that = this;
+      element.find('.read-less').on('click', function(e) {
+        e.preventDefault();
+        that.hideFullText(element);
+      });
+    },
+
+    showFullText: function(container, readMoreLink) {
+      var ellipsis = container.find('.read-more-hellip');
+      var hiddenTextContainer = container.find('.read-more-hidden');
+
+      readMoreLink.hide();
+      ellipsis.hide();
+      hiddenTextContainer.show();
+    },
+
+    hideFullText: function(container) {
+      var readMoreLink = container.find('.read-more');
+      var ellipsis = container.find('.read-more-hellip');
+      var hiddenTextContainer = container.find('.read-more-hidden');
+
+      readMoreLink.show();
+      ellipsis.show();
+      hiddenTextContainer.hide();
+    }
+  };
+
+  $.fn.readmore = function(options) {
+    new Readmore(this, options);
+  };
+}(jQuery));
